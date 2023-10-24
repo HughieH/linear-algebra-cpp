9f4a4539fd1feef2eaf180d935d9eecfa3d9db27
HughieH <wanhouwai@gmail.com>
2023-10-23 18:58:35 -0700

    added methods

--- linear_algebra.hpp
+++ linear_algebra.hpp
@@ -1,30 +1,118 @@
 #ifndef LINEAR_ALGEBRA_H
 #define LINEAR_ALGEBRA_H
 
-#include <vector>
 #include <iostream>
+#include <vector>
+#include <initializer_list>
 #include <stdexcept>
-#include <memory>
 
 namespace LinearAlgebra {
 
-template<typename T, size_t R, size_t C>
-
+template<typename Scalar> 
 class Matrix {
-   T* data; 
+
+protected:
+   
+   Scalar* matrix_data;
+   size_t rows;
+   size_t cols;
 
 public:
-   Matrix() : data(new T[R*C]) {}
 
-   ~Matrix(){ delete [] data; }
+   // Default Constructor
+   Matrix(size_t input_rows, size_t input_cols) 
+      : matrix_data(new Scalar[input_rows * input_cols]), rows(input_rows), cols(input_cols) {}
 
-   inline T* operator[](int row) {
-      return &data[row * C];
+   // Constructor for taking in vector representation, e.g. 
+   Matrix(size_t input_rows, size_t input_cols, std::initializer_list<std::initializer_list<Scalar>> init_list) 
+      : matrix_data(new Scalar[input_rows * input_cols]), rows(input_rows), cols(input_cols) {
+      
+      size_t row = 0;
+      for (const auto& curr_row : init_list) {
+         size_t col = 0;
+         for (const auto& elem : curr_row) {
+            if (row >= input_rows || col >= input_cols) {
+                throw std::out_of_range("Initializer list exceeds matrix dimensions");
+            }
+            matrix_data[row * cols + col] = elem;
+            ++col;
+         }
+         ++row;
+      }
+   }
+   
+   // Destructor
+   ~Matrix(){ delete [] matrix_data; }
+
+   // Getters for rows & cols
+   size_t getRows() const { return rows; }
+   size_t getCols() const { return cols; }
+
+   // Operator overload [][] 
+   inline Scalar* operator[](int row) {
+      if (row < 0 || static_cast<size_t>(row) >= rows) {
+         throw std::out_of_range("Row index out of bounds");
+      }
+      return &matrix_data[row * cols];
+}
+
+   inline const Scalar* operator[](int row) const {
+      return &matrix_data[row * cols];
    }
 
-   inline const T* operator[](int row) const {
-      return &data[row * C];
+   // Overloaded insertion operator for output
+   friend std::ostream& operator<< (std::ostream& os, const Matrix<Scalar>& matrix) {
+        for (size_t i = 0; i < matrix.rows; ++i) {
+            for (size_t j = 0; j < matrix.cols; ++j) {
+                os << matrix.matrix_data[i * matrix.cols + j] << " ";
+            }
+            os << std::endl;
+        }
+        return os;
+    }
+
+   // Overloaded multiplication operator for matrix multiplication
+   Matrix<Scalar> operator*(const Matrix<Scalar>& other_matrix) const {
+      
+      size_t other_cols = other_matrix.getCols();
+      
+      // Check if matrices are conformant.
+      if (rows != other_cols) { 
+         throw std::invalid_argument("Matrices are not conformant for multiplication");
+      }
+      
+      Matrix<Scalar> result(rows, other_cols);  // Resultant matrix
+      
+      for (size_t i = 0; i < rows; ++i) {
+         for (size_t j = 0; j < other_cols; ++j) {
+            result.matrix_data[i * other_cols + j] = 0; // Initialize result cell to 0
+            for (size_t k = 0; k < cols; ++k) {
+                  result.matrix_data[i * other_cols + j] += matrix_data[i * cols + k] * other_matrix.matrix_data[k * other_cols + j];
+            }
+         }
+      }
+      
+      return result;
    }
+
+   // Transpose our matrix
+   void transpose() {
+      Scalar* transposed_data = new Scalar[rows * cols];
+
+      for (size_t i = 0; i < rows; ++i) {
+         for (size_t j = 0; j < cols; ++j) {
+            transposed_data[j * rows + i] = matrix_data[i * cols + j];
+         }
+      }
+
+      // Swap the rows and columns values
+      std::swap(rows, cols);
+
+      // Cleanup old matrix_data and update pointer
+      delete[] matrix_data;
+      matrix_data = transposed_data;
+}
+
 };
 
 }  // End of namespace LinearAlgebra
--- test.cpp
+++ test.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
 #include "linear_algebra.hpp"
+#include "../eigen/Eigen/Dense"
 
-int main() {
-   LinearAlgebra::Matrix<int, 3, 3> mat;  // Create a 3x3 matrix of integers
-   mat[0][0] = 5;  // Assign a value to the top-left element
-   std::cout << mat[0][0] << std::endl;  // Print the top-left element
+using Eigen::MatrixXd;
+using LinearAlgebra::Matrix;
+ 
+int main()
+{
+   Matrix<int> a(2, 3, {
+      {1, 2, 3},
+      {3, 4, 3}
+   });
+   std::cout << a << std::endl; 
+   //a[1][2] = 5;
+   //std::cout << a[1][2] << std::endl; 
+   Matrix<int> b(3, 2, {
+      {1, 2},
+      {3, 4}, 
+      {5, 6}
+   });
+      
+   Matrix c = a * b;
+   std::cout << c << std::endl;  // Print the top-left element
    return 0;
 }
+   
+
+
+   /*
+   
+   */
